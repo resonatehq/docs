48d88037b641df74096d17c90236da73615cc060
Dominik Tornow <dominik.tornow@gmail.com>
2024-03-14 09:06:52 -0700

    Update docusaurus.config.ts
    Update baseURL from www to docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://www.resonatehq.io',
+  url: 'https://docs.resonatehq.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
