632bbbb45cadab9543297f21c3866b4c6e354984
David Farr <david@resonatehq.io>
2024-05-02 11:04:06 -0700

    Add link preview image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,8 +51,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    // image: 'img/docusaurus-social-card.jpg',
+    image: 'img/echo.png', // used for link previews
     colorMode: {
       defaultMode: 'light',
       disableSwitch: true,
