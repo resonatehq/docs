6b108f9bb2cc1b81e6d86c8cf58cf7fa4a159c42
Cully Wakelin <cully.wakelin@resonatehq.io>
2024-10-28 08:25:31 -0600

    restructure

---
+++ docs/get-started/python-quickstart/code/part-2/src/summarize/app.py
@@ -0,0 +1,32 @@
+# @@@SNIPSTART quickstart-py-part-1-app
+import random
+import time
+
+def downloadAndSummarize(ctx, url):
+    print("Downloading and summarizing content from", url)
+    # Download the content from the provided URL
+    content = yield ctx.lfc(download, url)
+    # Summarize the downloaded content
+    summary = yield ctx.lfc(summarize, content).with_options(promise_id="asdfsda")
+    # Return the summary
+    return summary
+
+def download(_, url):
+    print(f"Downloading data from {url}")
+    time.sleep(2.5)
+    # Simulate a failure to download data 50% of the time
+    if random.randint(0, 100) > 50:
+        print("Download failed")
+        raise Exception("Failed to download data")
+    print("Download successful")
+    return "This is the text of the page that was downloaded"
+    
+def summarize(_, content):
+    print("Summarizing content...")
+    time.sleep(2.5)
+    if random.randint(0, 100) > 50:
+        print("Summarization failed")
+        raise Exception("Failed to summarize content")
+    print("Summarization successful")
+    return "This is the summary of the page that was downloaded"
+# @@@SNIPEND
